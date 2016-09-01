f5188be46d6a6aa83c9b09f32dad8ac4a5b1c5a9
Fabian  Monsalve <fabtics@gmail.com>
2016-09-01 11:46:31 -0500

    Arreglando scripts

--- modificaciones.sql
+++
@@ -1,4 +0,0 @@
--- -----------------------------------------------------
--- Agregar campo cedula en la tabla profile
--- -----------------------------------------------------
-ALTER TABLE profile ADD idCard VARCHAR(15) NOT NULL AFTER idUser
