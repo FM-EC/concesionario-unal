53954ff3dce5b08541d7dbe4c547b1806f619059
monica <mppinedav@unal.edu.co>
2016-09-03 17:53:04 -0500

    se aniaden headers comunes para admin y seller

--- src/java/businessLogic/sessionManagement/AuthorizationFilter.java
+++ src/java/businessLogic/sessionManagement/AuthorizationFilter.java
@@ -36,10 +36,6 @@ public class AuthorizationFilter implements Filter {
             HttpSession ses = reqt.getSession(false);
 
             String reqURI = reqt.getRequestURI();
-//           Boolean rolOK = (ses != null && reqURI.contains("/" + ses.getAttribute("rol") + "/"));
-
-//            String pathinfo = reqt.getPathInfo();
-//            String context = reqt.getContextPath();
 
             if (reqURI.contains("/index.xhtml")
                     || reqURI.contains("/public/")
